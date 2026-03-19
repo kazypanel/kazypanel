11f386919c4590e6559e027a6a63a78c861be152
KazyPanel <kazylax1981@gmail.com>
2026-03-19 21:44:10 +0100

    Release v1.3.0

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * KazyPanel - Serveur Node.js
  * Gestion des domaines/sous-domaines Apache + PHP 8.4
  * Port: 8080
- * Dernière modification : 19/03/2026 21:42
+ * Dernière modification : 19/03/2026 21:44
  */
 
 const express = require('express');
